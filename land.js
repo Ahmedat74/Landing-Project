e491a403844749c7abc445647796c58416dbd783
Ahmedat74 <161038463+Ahmedat74@users.noreply.github.com>
2024-06-25 13:25:51 +0300

    first commit

---
+++ land.js
@@ -0,0 +1,9 @@
+let img = document.getElementById('img');
+let container =document.querySelector(".container");
+function phones(phone){
+    img.src= phone
+}
+function Colors(color){
+    container.style.background= color;
+
+}
